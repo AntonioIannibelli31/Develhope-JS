ed7acc724c06c58b00102d8fc4a3c45019c47683
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-07-17 19:16:30 +0200

    es21

---
+++ es21/es21.js
@@ -0,0 +1,15 @@
+let num1 = 10;
+let num2 = 20;
+// console.log("Before swap: ", num1, num2); // Before swap: 10 20
+
+let arr = [num1, num2];
+console.log(num1, num2);
+let [a, b] = [num1, num2];
+console.log(b, a);
+
+// // Variable swapping
+// let temp = num2;
+// num2 = num1;
+// num1 = temp;
+
+// console.log("After swap: ", num1, num2); // After swap: 20 10
