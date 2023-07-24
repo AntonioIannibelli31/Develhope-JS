6f4c5af8792bfe25dd4c71015f6d23722038478d
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-07-24 18:52:51 +0200

    es36

---
+++ es36/es36.js
@@ -0,0 +1,10 @@
+function repeatHello(callback) {
+  let id = setInterval(() => {
+    console.log(callback);
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(id);
+  }, 5000);
+}
+
+repeatHello("Hello");
